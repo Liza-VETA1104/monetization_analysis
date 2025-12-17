8ef46ff5121a97e4b37ba7989feaac41fc5fbe83
Liza-VETA1104 <emakovtseva@gmail.com>
2025-12-17 21:37:09 +0400

    Update activity_by_days_hours.sql

--- activity_by_days_hours.sql
+++ activity_by_days_hours.sql
@@ -1,3 +1,4 @@
+--активность по дням недели
 select
 	extract('isodow' from order_date) as day_of_week,
 	count(order_id) as orders_cnt
@@ -7,12 +8,24 @@ group by
 	day_of_week
 order by
 	day_of_week
-
+--активность по будням
 select
 	extract('hour' from order_timestamp) as hour_of_order,
 	count(order_id) as orders_cnt
 from
 	orders o
+where extract('isodow' from order_date) between 1 and 5
+group by
+	hour_of_order
+order by
+	hour_of_order
+--активность по выходным	
+select
+	extract('hour' from order_timestamp) as hour_of_order,
+	count(order_id) as orders_cnt
+from
+	orders o
+where extract('isodow' from order_date) in (6,7)
 group by
 	hour_of_order
 order by
